c0a77027863205fdc60fc59ec6ce11f870dceefe
Nadim <nadim.csm@gmail.com>
2026-08-09 16:36:11 +0600

    Implement FilamentUser authorization for production panels

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -14,10 +16,26 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
+
+    /**
+     * Determine if the user can access the given Filament panel in production.
+     */
+    public function canAccessPanel(Panel $panel): bool
+    {
+        if ($panel->getId() === 'admin') {
+            return $this->email === config('services.admin.email', 'admin@seraviva.com');
+        }
+
+        if ($panel->getId() === 'examiner') {
+            return Interviewer::where('user_id', $this->id)->exists();
+        }
+
+        return true;
+    }
 
     /**
      * Get the attributes that should be cast.
